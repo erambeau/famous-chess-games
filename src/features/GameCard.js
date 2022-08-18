f2bd128e742f7e8a7edb38f6c5c67ed1f9a2974f
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 15:38:40 +0200

    Add wrapping

--- src/features/GameCard.js
+++ src/features/GameCard.js
@@ -5,7 +5,7 @@ function GameCard() {
   return (
     <div className="game-card">
       <h2>Kasparov's Immortal</h2>
-      <img src={image} alt="Kasparov's Immortal" />
+      <img src={image} alt="Kasparov's Immortal" className="game-image" />
       <p><strong>Kasparov vs Topalov, 1999</strong></p>
       <p>(B07) Pirc, 44 moves, 1-0</p>
     </div>
