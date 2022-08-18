6a04c088afba5a99bca0f23433235ea43e671447
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 15:27:11 +0200

    Create a first version of the card

--- src/features/GameCard.js
+++ src/features/GameCard.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import image from "../images/positions/1011478.png"
 
 function GameCard() {
   return (
-    <div>GameCard</div>
+    <div className="game-card">
+      <h2>Kasparov's Immortal</h2>
+      <img src={image} alt="Kasparov's Immortal" />
+      <p><strong>Kasparov vs Topalov, 1999</strong></p>
+      <p>(B07) Pirc, 44 moves, 1-0</p>
+    </div>
   )
 }
 
--- src/layouts/MainContent.js
+++ src/layouts/MainContent.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import GameCard from '../features/GameCard'
 
 function MainContent() {
   return (
     <div className="main-part">
-      MainContent
+      <GameCard />
+      <GameCard />
+      <GameCard />
     </div>
   )
 }
