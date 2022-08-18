d7df99b7e967a0cb806af8f83163637ddcf40869
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 16:23:17 +0200

    Variabilize to display all games

--- src/layouts/MainContent.js
+++ src/layouts/MainContent.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import GameCard from '../features/GameCard'
+import data from '../data/chessgames_data'
+
 
 function MainContent() {
+
+  const cards = data.map(item => {
+    return (
+      <GameCard 
+                key={item.chessgameid}
+                {...item}
+      />
+    )
+  })
+
   return (
     <div className="main-part">
-      <GameCard />
-      <GameCard />
-      <GameCard />
+      {cards}
     </div>
   )
 }
