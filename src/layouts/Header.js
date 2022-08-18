68ee00f3d0f3b803fdfa8b02100e72ea99622954
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 16:23:03 +0200

    Fix image link

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import logo from "../images/pieces/Chess_qlt60.png"
 
 function Header() {
   return (
     <nav className="nav-header">
         <div className='nav-header--title'>
-            <img src={logo} className='nav-header--logo' />
+            <img src={process.env.PUBLIC_URL+"/images/pieces/Chess_qlt60.png"} className='nav-header--logo' />
             <h1>Famous chess games</h1>
         </div>
     </nav>
