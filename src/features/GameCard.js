ed252a71697bfe151c9132c3e632870a2b32f831
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 16:37:53 +0200

    Styling when hovering

--- src/features/GameCard.js
+++ src/features/GameCard.js
@@ -4,8 +4,8 @@ import React from 'react'
 function GameCard(props) {
   return (
     <div className="game-card">
-      <a href={"https://www.chessgames.com/perl/chessgame?gid=" + props.chessgameid} className="game-link">
-        <h2>{props.title}</h2>
+      <a href={"https://www.chessgames.com/perl/chessgame?gid=" + props.chessgameid} className="game-link" target="_blank">
+        <h2 className="game-title">{props.title}</h2>
         <img src={process.env.PUBLIC_URL + "/images/" + props.finalPosition} alt={props.title} className="game-image" />
         <p><strong>{props.player1} vs {props.player2}, {props.year}</strong></p>
         <p>{props.opening}, {props.nbMoves} moves, {props.score}</p>
