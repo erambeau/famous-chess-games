098bbbaaabf30eb9b3e8d1d0236d465f863b918f
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 16:22:46 +0200

    Variabilize

--- src/features/GameCard.js
+++ src/features/GameCard.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import image from "../images/positions/1011478.png"
 
-function GameCard() {
+
+function GameCard(props) {
   return (
     <div className="game-card">
-      <a href="https://www.chessgames.com/perl/chessgame?gid=1011478" className="game-link">
-        <h2>Kasparov's Immortal</h2>
-        <img src={image} alt="Kasparov's Immortal" className="game-image" />
-        <p><strong>Kasparov vs Topalov, 1999</strong></p>
-        <p>(B07) Pirc, 44 moves, 1-0</p>
+      <a href={"https://www.chessgames.com/perl/chessgame?gid=" + props.chessgameid} className="game-link">
+        <h2>{props.title}</h2>
+        <img src={process.env.PUBLIC_URL + "/images/" + props.finalPosition} alt={props.title} className="game-image" />
+        <p><strong>{props.player1} vs {props.player2}, {props.year}</strong></p>
+        <p>{props.opening}, {props.nbMoves} moves, {props.score}</p>
       </a>
     </div>
   )
 }
 
 export default GameCard
+
+
