7bdd18261cda6f22cbd041ba5bfa7ec37f427564
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 15:41:33 +0200

    Add the link to the game

--- src/features/GameCard.js
+++ src/features/GameCard.js
@@ -4,10 +4,12 @@ import image from "../images/positions/1011478.png"
 function GameCard() {
   return (
     <div className="game-card">
-      <h2>Kasparov's Immortal</h2>
-      <img src={image} alt="Kasparov's Immortal" className="game-image" />
-      <p><strong>Kasparov vs Topalov, 1999</strong></p>
-      <p>(B07) Pirc, 44 moves, 1-0</p>
+      <a href="https://www.chessgames.com/perl/chessgame?gid=1011478" className="game-link">
+        <h2>Kasparov's Immortal</h2>
+        <img src={image} alt="Kasparov's Immortal" className="game-image" />
+        <p><strong>Kasparov vs Topalov, 1999</strong></p>
+        <p>(B07) Pirc, 44 moves, 1-0</p>
+      </a>
     </div>
   )
 }
