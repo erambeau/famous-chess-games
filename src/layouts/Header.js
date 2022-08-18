e6151c430ef6fd39d5ce29ebdbdd45461d06729e
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-18 15:27:20 +0200

    Move image

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from "../images/pieces/Chess_qlt60.png"
 
 function Header() {
   return (
     <nav className="nav-header">
         <div className='nav-header--title'>
-            <img src="../images/pieces/Chess_qlt60.png" className='nav-header--logo' />
+            <img src={logo} className='nav-header--logo' />
             <h1>Famous chess games</h1>
         </div>
     </nav>
