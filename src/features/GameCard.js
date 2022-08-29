7c9a7d193b8d83f8c7cbc626af15a93c648b507b
erambeau <55458323+erambeau@users.noreply.github.com>
2022-08-29 23:41:54 +0200

    try removing & deploy to netlify

--- src/features/GameCard.js
+++ src/features/GameCard.js
@@ -6,7 +6,7 @@ function GameCard(props) {
     <div className="game-card">
       <a href={"https://www.chessgames.com/perl/chessgame?gid=" + props.chessgameid} className="game-link" target="_blank" rel="noreferrer">
         <h2 className="game-title">{props.title}</h2>
-        <img src={process.env.PUBLIC_URL + "/images/" + props.finalPosition} alt={props.title} className="game-image" />
+
         <p><strong>{props.player1} vs {props.player2}, {props.year}</strong></p>
         <p>{props.opening}, {props.nbMoves} moves, {props.score}</p>
       </a>
